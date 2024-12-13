a7c53d45d60a019a24982c93773a6edf860469b8
xzen769 <xzen769@gmail.com>
2024-12-13 10:30:57 +0100

    update

--- api/app/config.js
+++ api/app/config.js
@@ -2,11 +2,11 @@ module.exports = {
   development: {
     type: 'development',
     port: 3000,
-    mongodb: ''
+    mongodb: 'mongodb+srv://12345:12345@demo.v4muu5b.mongodb.net/e2e'
   },
   production: {
     type: 'production',
     port: 3000,
-    mongodb: ''
+    mongodb: 'mongodb+srv://12345:12345@demo.v4muu5b.mongodb.net/e2e'
   }
 }
